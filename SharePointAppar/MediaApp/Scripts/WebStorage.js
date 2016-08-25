5a64e84eeb7cc761a542553650773baae1d5e0b5
C. Andersson <hejjacarl@hotmail.com>
2016-08-25 15:52:16 +0200

    webstorage

---
+++ SharePointAppar/MediaApp/Scripts/WebStorage.js
@@ -0,0 +1,25 @@
+﻿'use strict'
+
+//TODO - fixa webbstorage!
+
+//var car = {};
+//car.wheels = 4;
+//car.doors = 2;
+//car.sound = 'vrooom';
+//car.name = "Lightning McQueen";
+
+//console.log(car);
+//localStorage.setItem("car", "car");
+
+//console.log(localStorage.getItem('car'));
+
+//if (localStorage.hits) {
+//    localStorage.hits = Number(localStorage.hits) + 1;
+//} else {
+//    localStorage.hits = 1;
+//}
+
+//document.write("Total Hits: " + localStorage.hits);
+
+//localStorage.setItem("car", JSON.stringify(car));
+//console.log(JSON.parse(localStorage.getItem("car")));
